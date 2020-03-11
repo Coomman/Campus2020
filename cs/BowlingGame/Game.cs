02e564f77a163b6cc0b1d0a32621f9634efca47e
Max <rauba.max9@mail.ru>
2020-03-11 10:32:04 +0300

    First commit

--- cs/BowlingGame/Game.cs
+++ cs/BowlingGame/Game.cs
@@ -7,25 +7,49 @@ namespace BowlingGame
 {
     public class Game
     {
+        private int score = 0; 
+
         public void Roll(int pins)
         {
+            score += pins;
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            return score;
         }
     }
 
     [TestFixture]
-    public class Game_should : ReportingTest<Game_should>
+    public class Game_Should : ReportingTest<Game_Should>
     {
+        private Game game;
+
+        [SetUp]
+        public void Setup()
+        {
+            game = new Game();
+        }
+
         [Test]
         public void HaveZeroScore_BeforeAnyRolls()
         {
-            new Game()
-                .GetScore()
-                .Should().Be(0);
+            game.GetScore().Should().Be(0);
+        }
+
+        [Test]
+        public void HavePinsScore_AfterFirstRoll()
+        {
+            game.Roll(7);
+            game.GetScore().Should().Be(7);
+        }
+
+        [Test]
+        public void HaveBothPinsScore_AfterOneFrame()
+        {
+            game.Roll(6);
+            game.Roll(3);
+            game.GetScore().Should().Be(9);
         }
     }
 }
