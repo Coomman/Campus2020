da702f282f3556082500fdb0c1e105c19cf0adeb
Max <rauba.max9@mail.ru>
2020-03-13 11:13:01 +0300

    GameBoard added

--- src/Controllers/MovesController.cs
+++ src/Controllers/MovesController.cs
@@ -15,6 +15,7 @@ namespace thegame.Controllers
             var game = TestData.AGameDto(userInput.ClickedPos ?? new Vec(1, 1));
             if (userInput.ClickedPos != null)
                 game.Cells.First(c => c.Type == "color4").Pos = userInput.ClickedPos;
+
             return new ObjectResult(game);
         }
     }
---
+++ src/GameObjects/GameBoard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace thegame.GameObjects
+{
+    public class GameBoard
+    {
+        private byte[][] _board;
+
+        public GameBoard(int width, int height)
+        {
+            _board = new byte[width][];
+            for (int i = 0; i < width; i++)
+            {
+                _board[i] = new byte[height];
+            }
+        }
+
+
+    }
+}
