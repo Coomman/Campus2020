451f26a2af3a8d6064542f59fd062a5f4c4aa763
Chazov Maxim <maximud.00@gmail.com>
2020-03-13 11:42:45 +0300

    Add Board Methods

--- src/GameObjects/GameBoard.cs
+++ src/GameObjects/GameBoard.cs
@@ -7,17 +7,44 @@ namespace thegame.GameObjects
 {
     public class GameBoard
     {
-        private byte[][] _board;
+        private int[,] _board;
 
         public GameBoard(int width, int height)
         {
-            _board = new byte[width][];
-            for (int i = 0; i < width; i++)
-            {
-                _board[i] = new byte[height];
-            }
+            _board = new int[width, height];
         }
 
+        public bool TryMoveUp()
+        {
+            throw new NotImplementedException();
+        }
+        public bool TryMoveLeft()
+        {
+            throw new NotImplementedException();
+        }
+        public bool TryMoveDown()
+        {
+            throw new NotImplementedException();
+        }
+        public bool TryMoveRight()
+        {
+            throw new NotImplementedException();
+        }
+        public bool GameOverCheck()
+        {
+            throw new NotImplementedException();
+        }
+        public void CreateGameCell()
+        {
+            var availableCells = new List<Tuple<int, int>>();
+            for (var i = 0; i < _board.GetLength(0); i++)
+            for (var j = 0; j < _board.GetLength(1); j++)
+                if (_board[i, j] == 0)
+                    availableCells.Add(Tuple.Create(i, j));
 
+            var rnd = new Random();
+            var (width, height) = availableCells[rnd.Next(availableCells.Count)];
+            _board[width, height] = rnd.Next(4) < 0 ? 1 : 2;
+        }
     }
 }
