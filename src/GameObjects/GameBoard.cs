c26cf3547a37249188fffa5cb572b85adf27479d
Chazov Maxim <maximud.00@gmail.com>
2020-03-13 12:48:46 +0300

    Add simple incorrect logic

--- src/GameObjects/GameBoard.cs
+++ src/GameObjects/GameBoard.cs
@@ -21,35 +21,86 @@ namespace thegame.GameObjects
         {
             _board = new int[width, height];
         }
-        public void MakeMove()
+
+        private static bool TryMoveArray(int[] row, bool isFake, out int[] shiftRow)
+        {
+            shiftRow = (int[]) row.Clone();
+            for (var i = row.Length - 1; i >= 0; i--)
+            {
+                if (row[i] == 0 && isFake)
+                    return true;
+                for (var j = i; j < row.Length - 1; j++)
+                {
+                    if (row[j] != row[j + 1]) continue;
+                    row[j + 1] += 1;
+                    shiftRow[j] = 0;
+                    if (isFake)
+                        return true;
+                }
+            }
+
+            return !isFake;
+        }
+
+        public bool MoveUp(bool isFake = false)
+        {
+            var localBoard = (int[,]) _board.Clone();
+            for (var i = 1; i < localBoard.GetLength(0); i++)
+            {
+                var row = new int[localBoard.GetLength(1)];
+                for (var j = 0; j < localBoard.GetLength(1); j++)
+                    row[j] = localBoard[j, i];
+
+                if (!TryMoveArray(row, isFake, out var shiftedRow)) continue;
+                if (isFake)
+                    return true;
+                for (var j = 0; j < localBoard.GetLength(1); j++)
+                    localBoard[j, i] = row[j];
+            }
+
+            if (isFake)
+                return false;
+            _board = (int[,]) localBoard.Clone();
+            return true;
+        }
+
+        public bool MoveLeft(bool isFake = false)
         {
             throw new NotImplementedException();
         }
 
-        public bool[] TryAllMoves()
+        public bool MoveDown(bool isFake = false)
+        {
+            var localBoard = (int[,]) _board.Clone();
+            for (var i = 1; i < localBoard.GetLength(0); i++)
+            {
+                var row = new int[localBoard.GetLength(1)];
+                for (var j = 0; j < localBoard.GetLength(1); j++)
+                    row[j] = localBoard[localBoard.GetLength(1) - j - 1, i];
+
+                if (!TryMoveArray(row, isFake, out var shiftedRow)) continue;
+                if (isFake)
+                    return true;
+                for (var j = 0; j < localBoard.GetLength(1); j++)
+                    localBoard[localBoard.GetLength(1) - j - 1, i] = row[j];
+            }
+
+            if (isFake)
+                return false;
+            _board = (int[,]) localBoard.Clone();
+            return true;
+        }
+
+        public bool MoveRight(bool isFake = false)
         {
             throw new NotImplementedException();
         }
-        public bool TryMoveUp()
-        {
-            throw new NotImplementedException();
-        }
-        public bool TryMoveLeft()
-        {
-            throw new NotImplementedException();
-        }
-        public bool TryMoveDown()
-        {
-            throw new NotImplementedException();
-        }
-        public bool TryMoveRight()
-        {
-            throw new NotImplementedException();
-        }
+
         public bool GameOverCheck()
         {
             throw new NotImplementedException();
         }
+
         public void CreateGameCell()
         {
             var availableCells = new List<Tuple<int, int>>();
@@ -59,8 +110,8 @@ namespace thegame.GameObjects
                     availableCells.Add(Tuple.Create(i, j));
 
             var rnd = new Random();
-            var (width, height) = availableCells[rnd.Next(availableCells.Count)];
-            _board[width, height] = rnd.Next(4) < 0 ? 1 : 2;
+            var (width, height) = availableCells[rnd.Next(availableCells.Count - 1)];
+            _board[width, height] = rnd.Next(4) != 0 ? 1 : 2;
         }
     }
 }
